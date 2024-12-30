d40b8010243a50ca9b12d69e6a06356c2b2a4531
Linko-3D <dbekhouc@gmail.com>
2024-12-30 19:58:53 +0100

    tweak

--- player/player.gd
+++ player/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var speed = 400.0
 
 
-func _ready() -> void:
+func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
 	%DisplayAuthority.visible = is_multiplayer_authority()
 
@@ -14,11 +14,5 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority(): return
 
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
 	move_and_slide()
